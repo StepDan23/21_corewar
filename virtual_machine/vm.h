925dd814db87311e74549905a87f4d1db6434cc7
Falmer shadowmaster Kuhn <fkuhn@e1r2p3.21-school.ru>
2019-03-31 16:31:16 +0300

    Begining of header file for vm

---
+++ virtual_machine/vm.h
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkuhn <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/31 16:14:39 by fkuhn             #+#    #+#             */
+/*   Updated: 2019/03/31 16:30:36 by fkuhn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VM_H
+# define VM_H
+
+/*
+*	Структура t_proccess представляет собой процесс 'каретку'
+*
+*	position - позиция каретки в памяти машины.
+*	carry - флаг, показывающий успешно ли выполнилась предыдущая операция
+*	этот флаг влияет на выполнение zjmp (выполнится если carry=1).
+*	player_id - номер игрока, который создал этот процесс.
+*	registers - регистры процесса.
+*	is_live - флаг, обозначающий жив ли процесс.
+*	command_type - тип комманды, которую надо выполнить.
+*	cycles_to_wait - кол-во циклов, которые каретке осталось ждать
+*	до выполнения операции.
+*/
+
+typedef struct		s_proccess
+{
+	int				position;
+	int				carry;
+	int				player_id;
+	unsigned int	registers[16];
+	int				is_live;
+	int				command_type;
+	int				cycles_to_wait;
+}					t_proccess;
+
+/*
+*	Декларации опкодов операций
+*/
+
+# define LIVE		1;
+# define LD			2;
+# define ST			3;
+# define ADD		4;
+# define SUB		5;
+# define AND		6;
+# define OR			7;
+# define XOR		8;
+# define ZJMP		9;
+# define LDI		10;
+# define STI		11;
+# define FORK		12;
+# define LLD		13;
+# define LLDI		14;
+# define LFORK		15;
+# define AFF		16;
+
+#endif
