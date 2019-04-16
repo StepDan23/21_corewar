05822f6abf449e37ebd5c2b3a605d73be570f035
How_r_u <kirtrishin@gmail.com>
2019-04-16 09:59:27 +0300

    фвв ашчуы

--- assembly/ft_lexer_champ_code.c
+++ assembly/ft_lexer_champ_code.c
@@ -6,7 +6,7 @@
 /*   By: how_r_u <how_r_u@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 18:46:59 by how_r_u           #+#    #+#             */
-/*   Updated: 2019/04/15 23:50:13 by how_r_u          ###   ########.fr       */
+/*   Updated: 2019/04/16 00:24:25 by how_r_u          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,46 @@
 	имя команды,
 	ее атрибуты,
 	метки,
-	! Проверять тип элемента последовательно - сначала на пробелб потом на метку, потом на r1,
+	! Проверять тип элемента последовательно - сначала на пробел потом на метку, потом на r1,
 	! потом %, потом число потом, на комманду. Сделать паровоз из функций
 */
 
-int	ft_get_word_len(char *line)
+int		ft_find_sym(char *line, char *sym)
+{
+	int		i;
+
+	i = 0;
+	while (line[i] && line[i] != sym && line[i] != COMMENT_CHAR)
+		i++;
+	if (line[i] && line[i] != COMMENT_CHAR)
+		return (i);
+	else
+		return (-1);
+}
+
+int		ft_check_label_sym(char *line)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (line[i] && line[i] != ':')
+	{
+		j = 0;
+		while (LABEL_CHARS[j])
+		{
+			if (LABEL_CHARS[j] = line[i])
+				break ;
+			j++;
+		}
+		if (!LABEL_CHARS[j])
+			return (0);
+	}
+	return (1);
+}
+
+int		ft_get_word_len(char *line)
 {
 	int	i;
 
@@ -41,9 +76,37 @@ int	ft_get_word_len(char *line)
 	return (0);
 }
 
+void	ft_fill_token2(t_asm_data *asm_data, char *line)
+{
+	if (line[0] == 'r')
+	{
+		//если строка число то все ок, если нет то добавляем ошибку
+	}
+}
+
 void	ft_fill_token(t_asm_data *asm_data, char *line)
 {
 	ft_printf("%s\n", line);
+	TOKEN_DATA->str = line;
+	//здесь определение типов токенов
+	if (line[0] == ' ' || line[0] == '\t')
+	{
+		TOKEN_DATA->type = Whitespace;
+		return ;
+	}
+	if (ft_find_sym(line, LABEL_CHAR))
+	{
+		if (ft_check_label_sym(line))
+			TOKEN_DATA->type = Label;
+		else
+		{
+			TOKEN_DATA->type = Unknown;
+			ft_error_add(asm_data, ft_strjoin("Wrong symbols in label ", line),\
+			 TOKEN_DATA->col, TOKEN_DATA->row);
+		}
+		return ;
+	}
+	ft_fill_token2(asm_data, line);
 }
 
 char	*ft_lexer_champ_code(t_asm_data *asm_data, char *line)
